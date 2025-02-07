ed06e39647be0c1f362678cd67a18c837ac40a13
5690 <5690Programmers@gmail.com>
2025-02-06 18:48:28 -0600

    commited plagerism of myself (added coral arm skeleton)

--- src/main/include/constants/ArmConstants.h
+++ src/main/include/constants/ArmConstants.h
@@ -43,3 +43,37 @@ namespace AlgaeArmConstants{
 
 }
 
+namespace CoralArmConstants{
+    constexpr int kArmMotorId = -1;
+    constexpr int kIntakeMotorId = 20;
+    constexpr double kP = -1;
+    constexpr double kI = -1;
+    constexpr double kD = -1;
+    constexpr double kIZone = -1;
+    constexpr double kFF = -1;
+    
+    constexpr units::revolutions_per_minute_t kMaxRpm = 1_rpm;
+    constexpr units::degree_t kHomeRotation = 10_deg;
+    constexpr units::degree_t kMaxRotation = 190_deg;
+    constexpr units::degree_t kRelativeDistancePerRev = 360_deg * (1 / 8.75);
+    constexpr units::degree_t kAbsoluteDistancePerRev = 360_deg;
+    constexpr units::degrees_per_second_t kDefaultVelocity = 10_deg_per_s;
+    constexpr double kVelocityScalar = 1.0;
+    constexpr units::degree_t kTolerance = 2_deg;
+    constexpr units::meter_t kArmLength = 0.2_m;
+    
+    static const subzero::SingleAxisMechanism kCoralArmMechanism = {
+    // length
+    0.2_m,
+    // min angle
+    110_deg,
+    // line width
+    6,
+    // color
+    subzero::ColorConstants::kBlue};
+
+    const frc::TrapezoidProfile<units::degree>::Constraints
+        kRotationalAxisConstraints{360_deg_per_s * 2.2, 360_deg_per_s_sq * 2.2};
+
+}
+
